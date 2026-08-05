d5a9edf2740b680fe1facc6bfe61788c76d7c6be
ayeshahassan22 <ayeshahasan2112@gmail.com>
2026-08-05 08:36:31 -0700

    Format server.js for better readability

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const path = require("path");
   app.use(express.urlencoded({ extended: true })); 
   
   // Website files
- app.use(express.static(__dirname,)); 
+ app.use(express.static(__dirname)); 
     
   app.get("/", (req, res) => { res.sendFile(path.join(__dirname,  "index.html")); });
 app.get("/test", (req, res) => { res.send("Server is working"); });
